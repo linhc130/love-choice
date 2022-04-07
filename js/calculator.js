6d2ef1d1122640f43d28c24e95a578ae8c8c53cd
Stephen Lin <linhc130@hotmail.com>
2022-04-07 20:11:00 +0800

    update html output wording in calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -7,14 +7,14 @@ function cal() {
     var finalPrice = Math.round(totalPrice + num * 300 )
 
     var text = ``
-if (Shipping == 0 || (Shipping !== Shipping)) {
-        text += `<span class="Price">估算應付金額務必輸入請 代購件數 (最終應付金額仍由小幫手確認訂單方能成立)<span>`
+if (Shipping == 0 || (Shipping !== Shipping) || (price !== price) || (num !== num)) {
+        text += `<span class="Price">估算應付金額務必輸入請 代購商品件數、商品總金額、預估重量(kg)<span>`
     } else { 
-        text += `<p>📣報價如下</br>🔷商品金額：[<span class="Price">${toCurrency(price)}</span> * <span class="forex">${toCurrency(forex)}</span> + <span class="Shipping">${toCurrency(Shipping)}</span>(代購件數) * 50]` 
+        text += `<p>📣報價如下</br>🔷商品金額：<span class="Price">${toCurrency(price)}</span> * <span class="forex">${toCurrency(forex)}</span> + (代購件數)<span class="Shipping">${toCurrency(Shipping)}</span> * 50` 
         text += ` = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>
 🔷國際運費：1公斤300元</br>
-🔷估算應付金額：<span class="totalPrice">${toCurrency(totalPrice)}</span> + <span class="Num">300*${num}</span>(重量預估${num}kg以內) = <span class="finalPrice">${toCurrency(finalPrice)}</span></p>` }
-
+🔷估算應付金額：<span class="totalPrice">${toCurrency(totalPrice)}</span> + <span class="Num">300*${num}</span>(重量預估${num}kg以內) = <span class="finalPrice">${toCurrency(finalPrice)}</span></br>
+(最終應付金額仍由小幫手確認後，訂單方能成立)</p>` }
 
     $(".text").html(text)
 if (Shipping != 0 && (Shipping == Shipping)) {
